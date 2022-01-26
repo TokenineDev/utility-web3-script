6bdb0c1c9b0a0d0c7bf7ac169b1f2485920f2943
Teerapon Nampakdee <teerapon@kkumail.com>
2022-01-26 09:40:16 +0700

    upgrade script

--- send_erc20.js
+++ send_erc20.js
@@ -1,48 +1,55 @@
-import { ethers } from 'ethers'
-import dotenv from 'dotenv'
+import { BigNumber, ethers } from "ethers";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const pKey = process.env.PRIVATE_KEY
+const pKey = process.env.PRIVATE_KEY;
 
 const erc20ABI = [
-    'function name() view returns (string)',
-    'function symbol() view returns (string)',
-    'function balanceOf(address) view returns (uint)',
-    'function transfer(address to, uint amount)',
-    'event Transfer(address indexed from, address indexed to, uint amount)'
+  "function name() view returns (string)",
+  "function symbol() view returns (string)",
+  "function balanceOf(address) view returns (uint)",
+  "function transfer(address to, uint amount)",
+  "event Transfer(address indexed from, address indexed to, uint amount)",
 ];
 
-const tokenAddress = '0x9c882a7004D4bB7E5fa77856625225EA29619323'
+const tokenAddress = "0xe57EDc546Ee99f17a5d6e32C2457B4569ecD40f8";
 
-const wallet = new ethers.Wallet(pKey)
-const provider = new ethers.providers.JsonRpcProvider('https://rpc.xchain.asia')
-const signer = wallet.connect(provider)
-const tokenContract = new ethers.Contract(tokenAddress, erc20ABI, signer)
+const wallet = new ethers.Wallet(pKey);
+const provider = new ethers.providers.JsonRpcProvider(
+  "https://bsc-dataseed.binance.org/"
+);
+const signer = wallet.connect(provider);
+const tokenContract = new ethers.Contract(tokenAddress, erc20ABI, signer);
 
 // gas limit use in transfer token abount 75000 Unit.
 // One transfer will use gas in xchain 20 gwei * 75000 = 1500000 gwei.
 // Then, one transfer will use gas about 0.0015 XTH
 
-const wallets = [
-  {
-    address: '0x626939e224FbD56F5DE5b244dC04f8a1cEF40613',
-    amount: '100'
-  },
-]
+const wallets = [];
 
 const runTransfer = async () => {
-  try {
-    for (const wallet of wallets) {
+  console.log("Total wallet:", wallets.length);
+  let totalAmount = BigNumber.from(0);
+  for (const wallet of wallets) {
+    try {
+      const amount = ethers.utils.parseEther(wallet.amount);
+      totalAmount = totalAmount.add(amount);
       const response = await tokenContract.transfer(
         wallet.address,
-        ethers.utils.parseEther(wallet.amount) // change ether to wei unit
-      )
-      console.log(response)
+        amount // change ether to wei unit
+      );
+      console.log("Start:", wallet.address);
+      console.log("Amount:", wallet.amount);
+      await response.wait();
+      console.log("Done");
+    } catch (err) {
+      console.log("Error at:", wallet.address);
+      console.error(err);
     }
-  } catch (err) {
-    console.error(err)
   }
-}
+  console.log("WEI:", totalAmount.toString());
+  console.log("ETHER:", ethers.utils.formatEther(totalAmount));
+};
 
-runTransfer()
+runTransfer();
